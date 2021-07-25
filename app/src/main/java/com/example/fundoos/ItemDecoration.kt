4dccfc42d2ee23226e6ea078c3f5c1eb6f3d5ee7
prudhvi <prudhvieethakota123@gmail.com>
2021-07-26 00:04:30 +0530

    Code for implementing item decoration in the state as recycler view

--- app/src/main/java/com/example/fundoos/ItemDecoration.kt
+++ app/src/main/java/com/example/fundoos/ItemDecoration.kt
@@ -5,8 +5,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class TopSpacingItemDecoration(private val padding: Int): RecyclerView.ItemDecoration() {
-    override fun getItemOffsets(
-        outRect: Rect,
+    override fun getItemOffsets( outRect: Rect,
         view: View,
         parent: RecyclerView,
         state: RecyclerView.State
