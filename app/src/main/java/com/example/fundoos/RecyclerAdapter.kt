5fc27c574f359ec6fe26baa3a61111a7beebc135
prudhvi <prudhvieethakota123@gmail.com>
2021-07-29 07:50:17 +0530

    Code for Update and Delete the SQLite DataBase By Creating DialogBox  from the Recycler View

--- app/src/main/java/com/example/fundoos/RecyclerAdapter.kt
+++ app/src/main/java/com/example/fundoos/RecyclerAdapter.kt
@@ -3,19 +3,23 @@ package com.example.fundoos
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.PopupMenu
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_create_new_note.*
+import kotlinx.android.synthetic.main.fragment_create_new_note.view.*
+import kotlinx.android.synthetic.main.fragment_update_note.*
 import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
+import java.text.SimpleDateFormat
 
 class RecyclerAdapter(
     val context: Context,
-    private var notes: MutableList<CreatingNewNotes> = mutableListOf()
+    private var notes: MutableList<Notes> = mutableListOf()
 ) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -24,40 +28,12 @@ class RecyclerAdapter(
         private var notesTitle: TextView = notesView.recycler_title
         private var notesDate: TextView = notesView.recycler_date
         private var notesContent: TextView = notesView.recycler_notes
-        var menuItem: ImageView = notesView.recycler_Button
+        var menuItem: ImageButton = notesView.recycler_Button
 
-        init {
-            menuItem.setOnClickListener() { popupMenu(it) }
-        }
-
-        private fun popupMenu(view: View?) {
-            val popupMenu = PopupMenu(context.applicationContext, view)
-            popupMenu.inflate(R.menu.recycler_menu_items)
-            popupMenu.setOnMenuItemClickListener {
-                when (it.itemId) {
-                    R.id.recycler_delete -> {
-//                        deleteRecordAlertDialog(n)
-                        Toast.makeText(context, "The Selected Notes Deleted", Toast.LENGTH_SHORT)
-                            .show()
-                        true
-                    }
-                    else -> true
-                }
-                popupMenu.show()
-                val popup = PopupMenu::class.java.getDeclaredField("popupMenu")
-                popup.isAccessible = true
-                val menu = popup.get(popupMenu)
-                menu.javaClass.getDeclaredMethod("setForceShowIcon", Boolean::class.java)
-                    .invoke(menu, true) as Boolean
-
-            }
-        }
-
-        fun bind(notesPost: CreatingNewNotes) {
+        fun bind(notesPost: Notes) {
             notesTitle.text = notesPost.title
             notesDate.text = notesPost.dateTime
             notesContent.text = notesPost.notes
-
         }
 
         override fun onClick(v: View?) {
@@ -73,33 +49,106 @@ class RecyclerAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
             is NotesViewHolder -> {
-                holder.bind(notes.get(position))
+                holder.bind(notes[position])
+                holder.menuItem.setOnClickListener() {
+                    popupMenu(it, notes[position])
+                }
+//                holder.itemView.setOnClickListener() {
+//                    val updateNoteFragment: UpdateNoteFragment ?= null
+//                    var args: Bundle ?= null
+//                    args?.putString("Your key", "Your value")
+//                    updateNoteFragment?.arguments = args
+//
+//                }
             }
         }
-        holder.itemView.setOnClickListener() {
+    }
+
+    private fun popupMenu(view: View?, notes: Notes) {
+        val popupMenu = PopupMenu(context, view)
+        popupMenu.inflate(R.menu.recycler_menu_items)
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+
+                R.id.recycler_edit -> {
+                    editRecordAlertDialog(notes)
+                    Toast.makeText(context, "The Selected Notes Edited", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.recycler_delete -> {
+                    deleteRecordAlertDialog(notes)
+                    Toast.makeText(context, "The Selected Notes Deleted", Toast.LENGTH_SHORT)
+                        .show()
+                    notifyDataSetChanged()
+                    true
+                }
+                else -> false
+            }
 
         }
-
+        popupMenu.show()
     }
 
-    override fun getItemCount(): Int {
-        return notes.size
+    private fun editRecordAlertDialog(notes: Notes) {
+        var currentDate: String? = null
+        val inflater = LayoutInflater.from(context)
+        val subView = inflater.inflate(R.layout.fragment_create_new_note, null)
+        val titleField: EditText = subView.New_Note_Title
+        val contentField: EditText = subView.New_Note_Content
+        val dateTimeField: TextView = subView.create_newNote_dateTime
+        val dateAndTime = SimpleDateFormat("dd/mm/yyyy hh:mm:ss")
+        currentDate = dateAndTime.format(java.util.Date())
+        dateTimeField.text = currentDate
+        titleField.setText(notes.title)
+        contentField.setText(notes.notes)
+
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Edit Notes")
+        builder.setView(subView)
+        builder.create()
+        builder.setPositiveButton("Update") { _, _ ->
+
+            val dataBaseHandler: DataBaseHandler? = context?.let { DataBaseHandler(it) }
+            var title = titleField.text.toString()
+            var content = contentField.text.toString()
+            var dateTime = dateTimeField.text.toString()
+
+            if (TextUtils.isEmpty(title) && TextUtils.isEmpty(content)) {
+                Toast.makeText(context, "Please Enter the Fields", Toast.LENGTH_SHORT).show()
+            } else {
+                val status = dataBaseHandler?.updateNotes(notes.id, title, content, dateTime)
+                if (status != null) {
+                    if (status > -1) {
+                        Toast.makeText(context, "Data Successfully submitted", Toast.LENGTH_SHORT)
+                            .show()
+                        titleField.text.clear()
+                        contentField.text.clear()
+                    }
+                }
+
+                (context as HomeActivity).finish()
+                context.startActivity(context.intent)
+            }
+
+        }
+        builder.setNegativeButton("Cancel") { _, _ ->
+            Toast.makeText(context, "Task Cancelled", Toast.LENGTH_SHORT).show()
+
+        }
+        builder.show()
     }
 
-    fun submitList(notesList: MutableList<CreatingNewNotes>) {
-        notes = notesList
-    }
-
-    fun deleteRecordAlertDialog(notes: CreatingNewNotes) {
+    private fun deleteRecordAlertDialog(notes: Notes) {
         val builder = AlertDialog.Builder(context)
         //set title for alert dialog
         builder.setTitle("Delete Note Record")
         builder.setMessage("Are you sure You wants to delete ${notes.id}")
         builder.setIcon(android.R.drawable.ic_dialog_alert)
 
-        builder.setPositiveButton("Yes") { dialogInterface, which ->
+        builder.setPositiveButton("Ok") { dialogInterface, which ->
             val dataBaseHandler: DataBaseHandler? = context?.let { DataBaseHandler(it) }
-            val status = dataBaseHandler?.deleteNotes(CreatingNewNotes(notes.id, "", "", ""))
+            val status = dataBaseHandler?.deleteNotes(Notes(notes.id, "", "", ""))
             if (status != null) {
                 if (status > -1) {
                     Toast.makeText(context, "Record Delete Successfully", Toast.LENGTH_SHORT).show()
@@ -107,39 +156,23 @@ class RecyclerAdapter(
 
                 dialogInterface.dismiss()
             }
-            builder.setNegativeButton("No") { dialogInterface, which ->
-                dialogInterface.dismiss()
-            }
-            val alertDialog: AlertDialog = builder.create()
-
-            alertDialog.setCancelable(false)
-            alertDialog.show()
         }
+        builder.setNegativeButton("Cancel") { dialogInterface, which ->
+            dialogInterface.dismiss()
+        }
+        val alertDialog: AlertDialog = builder.create()
+
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 
-    class NotesItemDiffCallback(
-        var oldNotesList: MutableList<CreatingNewNotes>,
-        var newNotesList: MutableList<CreatingNewNotes>
-    ) : DiffUtil.Callback() {
-        override fun getOldListSize(): Int {
-            return oldNotesList.size
-        }
-
-        override fun getNewListSize(): Int {
-            return newNotesList.size
-        }
-
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return (oldNotesList.get(oldItemPosition).id == newNotesList.get(newItemPosition).id)
-        }
-
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldNotesList.get(oldItemPosition).equals(newNotesList.get(newItemPosition))
-        }
-
+    override fun getItemCount(): Int {
+        return notes.size
     }
 
-
+    fun submitList(notesList: MutableList<Notes>) {
+        notes = notesList
+    }
 }
 
 
